11bedc33772d690dbf0c04ead953094865701a98
Diego Eiras <eiras.lucio@gmail.com>
2020-11-27 18:07:56 -0300

    📦 ADD: ESLint, Prettier and EditorConfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import GlobalStyles from './styles/global';
 
-const App = () => {
-  return (
-    <>
-      <h1>Hello World</h1>
-      <GlobalStyles />
-    </>
-  );
-}
+const App = () => (
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyles />
+  </>
+);
 
 export default App;
