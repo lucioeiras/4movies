0324954801074c05fcc0b061ba330e87efc4c378
Diego Eiras <eiras.lucio@gmail.com>
2020-11-28 11:29:54 -0300

    📦 ADD: Connect search page to the api

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,23 +1,51 @@
+import { useState, useCallback, FormEvent } from 'react';
 import { FaSearch } from 'react-icons/fa';
+
+import Movie from '../../types/movie';
 
 import PageContent from '../../components/PageContent';
 import Sidebar from '../../components/Sidebar';
 import MoviesList from '../../components/MoviesList';
 
-import { Container, SearchBar } from './styles';
+import { Container, Form } from './styles';
 
 const Search = () => {
+  const [results, setResults] = useState<Movie[]>();
+  const [query, setQuery] = useState('');
+
+  const searchMovies = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+
+      if (query) {
+        fetch(
+          `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}`,
+        )
+          .then(response => response.json())
+          .then(response => setResults(response.results));
+      }
+    },
+    [query],
+  );
+
   return (
     <Container>
       <Sidebar page="search" />
 
       <PageContent>
-        <SearchBar>
-          <FaSearch size={20} />
-          <input placeholder="Type something cool" />
-        </SearchBar>
+        <Form onSubmit={searchMovies}>
+          <input
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+            placeholder="Type something cool"
+          />
 
-        <MoviesList title="Results" />
+          <button type="submit">
+            <FaSearch size={20} />
+          </button>
+        </Form>
+
+        {results && <MoviesList title="Results" movies={results} />}
       </PageContent>
     </Container>
   );
