823fc154deba7c468e89c88ac465fa64f9039361
Diego Eiras <eiras.lucio@gmail.com>
2020-11-28 01:22:40 -0300

    📦 ADD: Details page layout

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,13 +1,53 @@
+import PageContent from '../../components/PageContent';
 import Sidebar from '../../components/Sidebar';
+import MovieInfo from '../../components/MovieInfo';
+import MoviesList from '../../components/MoviesList';
 
-import { Container } from './styles';
+import { Container, Reviews, Review } from './styles';
 
 const Details = () => {
   return (
     <Container>
       <Sidebar />
 
-      <h1>Details</h1>
+      <PageContent>
+        <MovieInfo />
+
+        <Reviews>
+          <h2>Reviews</h2>
+
+          <div>
+            <Review>
+              <h3>Dan Harbor</h3>
+              <p>
+                “The work of billionaire tech CEO Donovan Chalmers is so
+                valuable that he hires mercenaries to protect it, and a
+                terrorist group kidnaps his daughter just to get it.”
+              </p>
+            </Review>
+
+            <Review>
+              <h3>Dan Harbor</h3>
+              <p>
+                “The work of billionaire tech CEO Donovan Chalmers is so
+                valuable that he hires mercenaries to protect it, and a
+                terrorist group kidnaps his daughter just to get it.”
+              </p>
+            </Review>
+
+            <Review>
+              <h3>Dan Harbor</h3>
+              <p>
+                “The work of billionaire tech CEO Donovan Chalmers is so
+                valuable that he hires mercenaries to protect it, and a
+                terrorist group kidnaps his daughter just to get it.”
+              </p>
+            </Review>
+          </div>
+        </Reviews>
+
+        <MoviesList title="Similar Movies" />
+      </PageContent>
     </Container>
   );
 };
--- src/pages/Details/styles.ts
+++ src/pages/Details/styles.ts
@@ -1,7 +1,53 @@
 import styled from 'styled-components';
 
+import colors from '../../styles/colors';
+
 export const Container = styled.div`
   height: 100vh;
-
   display: flex;
+`;
+
+export const Reviews = styled.section`
+  width: 100%;
+  max-width: 112rem;
+
+  border-radius: 2.4rem;
+  background: ${colors.card};
+
+  padding: 4.8rem;
+  margin-top: 6.4rem;
+
+  > h2 {
+    color: ${colors.primary};
+    font-size: 4rem;
+  }
+
+  > div {
+    margin-top: 32px;
+  }
+
+  @media (max-width: 700px) {
+    > div {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+`;
+
+export const Review = styled.div`
+  & + & {
+    margin-top: 3.2rem;
+  }
+
+  > h3 {
+    color: ${colors.title};
+    font: 500 2.4rem 'Poppins';
+  }
+
+  > p {
+    color: ${colors.text};
+    font: 400 1.6rem 'Poppins';
+    font-style: italic;
+
+    margin-top: 1.2rem;
+  }
 `;
